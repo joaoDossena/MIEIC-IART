704ab1f7e7080bf4bdcbd949782c2ebfc53bc987
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-17 18:02:13 +0000

    Start of Conversion of movable pieces to objects of a class to avoid tuple complications

---
+++ src/movablePiece.py
@@ -0,0 +1,17 @@
+class movablePiece:
+
+    def __init__(self, symbol, startingRow, startingCol):
+        self.symbol = symbol
+        self.row = startingRow
+        self.col = startingCol
+
+    def updatePos(self, newRow, newCol):
+        self.row = newRow
+        self.col = newCol
+
+
+# # Testing
+# p = movablePiece(".", 1, 2)
+# print("{} {}".format(p.row, p.col))
+# p.updatePos(5, 4)
+# print("{} {}".format(p.row, p.col))
