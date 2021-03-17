ae21a0df649db13487ced9a4d0ba3770a2a673f3
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2021-03-17 16:38:22 +0000

    Added function that returns farthest coords resulting from a horizontal/vertical move

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+# Given a board and a direction (either 1 or 0 in rowMov and colMov) returns the farthest position in that direction
+def getNewPiecePosition(board, curRow, curCol, rowMov, colMov):
+    if (rowMov == 0 and colMov == 0): return [curRow, curCol]
+
+    newRow = curRow
+    newCol = curCol
+
+    while (True):
+        print("{} {}".format(newRow, newCol))
+        if (newRow + rowMov >= 0 and newRow + rowMov < len(board) and newCol + colMov >= 0 and newCol + colMov < len(board)):
+            if (board[newRow + rowMov][curCol + colMov] != "."):
+                break # se o mov for para uma casa que não vazia
+            else: # else muda para lá a pos atual
+                newRow += rowMov
+                newCol += colMov
+        else:
+            break      
+
+    return [newRow, newCol]
+
+
+# board = [
+#     [".", "/", "."],
+#     [".", ".", "."],
+#     [".", ".", "."]
+# ]
+
+# startRow = 2
+# startCol = 0
+
+# [endRow, endCol] = getNewPiecePosition(board, startRow, startCol, 0, 0)
+# print("Start Position: {} {}".format(startRow, startCol))
+# print("End   Position: {} {}".format(endRow, endCol))
