d789c6bae621afb936c269f3abdcf7689dc501cc
joaoDossena <joaopdossena@gmail.com>
2021-04-30 09:57:19 +0100

    Adding comment

--- proj2 - Irony Detection/src/main.py
+++ proj2 - Irony Detection/src/main.py
@@ -8,8 +8,8 @@ df = pd.read_csv("./datasets/train/train-taskA.txt", sep="	")
 # indices nao lidos (inclusive): 1646-1648, 3029-3039, 3459-3461
 for i in range(1, len(df)):
 	if(df['Tweet index'][i] != (df['Tweet index'][i-1] + 1)):
-		print("Here")
 		print("Current", df['Tweet index'][i])
 		print("Previous", df['Tweet index'][i-1])
 
 
+# TODO: tokenizar os tuítes
